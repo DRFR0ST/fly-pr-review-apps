019f13703d5d7cf39b783b37eed2a1e0e4022cb6
Mike Eling <mike.eling97@gmail.com>
2023-12-01 00:07:19 +0100

    Experimentally test using flyctl deploy instead of flyctl machine

--- entrypoint.sh
+++ entrypoint.sh
@@ -70,11 +70,12 @@ if ! flyctl status --app "$app"; then
   fi
 
   # Assign a public IPv4 address to the app.
-  flyctl ips allocate-v4 --shared --app "$app"
-  flyctl ips allocate-v6 --app "$app"
+  # flyctl ips allocate-v4 --shared --app "$app"
+  # flyctl ips allocate-v6 --app "$app"
 
   # flyctl deploy --config "$config" --dockerfile "$dockerfile" $build_arg --app "$app" --region "$region" --image "$image" --vm-size "$vm_size" --vm-memory "$vm_memory" --strategy immediate --wait-timeout "$wait_timeout"
-  flyctl machine run $image --dockerfile "$dockerfile" --app "$app" --region "$region" --vm-size "$vm_size" --vm-memory "$vm_memory" --autostart --restart "on-fail"
+  flyctl deploy --dockerfile "$dockerfile" --app "$app" --region "$region" --vm-size "$vm_size" --vm-memory "$vm_memory"
+  # flyctl machine run $image --dockerfile "$dockerfile" --app "$app" --region "$region" --vm-size "$vm_size" --vm-memory "$vm_memory" --autostart --restart "on-fail"
 elif [ "$INPUT_UPDATE" != "false" ]; then
   # flyctl deploy --config "$config" --dockerfile "$dockerfile" $build_arg --app "$app" --region "$region" --image "$image" --vm-size "$vm_size" --vm-memory "$vm_memory" --strategy immediate --wait-timeout "$wait_timeout"
   flyctl machine run $image --dockerfile "$dockerfile" --app "$app" --region "$region" --vm-size "$vm_size" --vm-memory "$vm_memory" --autostart --restart "on-fail"
